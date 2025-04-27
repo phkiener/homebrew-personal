279e4e455dcee2aa825a4b8768d1c648b095300b
Philipp Kiener <kiener.phil@gmail.com>
2025-04-27 12:20:42 +0200

    colima-status-bar: Update to 2.1.0

--- Casks/colima-status-bar.rb
+++ Casks/colima-status-bar.rb
@@ -1,11 +1,11 @@
 cask "colima-status-bar" do
-    version "2.0.1"
-    sha256 "e4ca0748b6d83bee7263258f16254d750ee30ef279bef596d0b0272177f4fccf"
+    version "2.1.0"
+    sha256 "af3cd9c34698d621a0a60e5514e4aa3421623cc03598a7aae0992e87e1478a63"
     
-    url "https://github.com/phkiener/ColimaStatusBar/releases/download/#{version}/ColimaStatusBar.dmg"
+    url "https://github.com/phkiener/ColimaStatusBar/releases/download/#{version}/ColimaStatusBar-#{version}.tgz"
         
     name "colima-status-bar"
-    desc "A tine menubar icon to display the status of colima"
+    desc "A tiny menubar icon to display the status of colima"
     homepage "https://github.com/phkiener/ColimaStatusBar/"
     
     livecheck do
