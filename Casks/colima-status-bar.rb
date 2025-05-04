9b47862d63bbc2b0208853360168ca74d82453b6
Philipp Kiener <kiener.phil@gmail.com>
2025-05-04 12:36:35 +0200

    colima-status-bar: Fix hash after changing the zip file

--- Casks/colima-status-bar.rb
+++ Casks/colima-status-bar.rb
@@ -1,6 +1,6 @@
 cask "colima-status-bar" do
     version "3.0.0"
-    sha256 "C22DE5B81FB82D9EA207E8E7FF19BA78E803FF5F8007A83FD351C4718F8837F6"
+    sha256 "98F190C3398B395EF7A3632C31BC84F091AF3D0B17A5DA1E5C88A326AB012307"
     
     url "https://github.com/phkiener/ColimaStatusBar/releases/download/#{version}/ColimaStatusBar-#{version}.zip"
         
